76fd61ab9cb887f7025c995bdeee6b316b212967
vkub1 <kubliyvlad@gmail.com>
2020-11-19 18:54:17 -0800

    Fixed some missing files

---
+++ app/src/main/java/com/example/covidcaster/HelpActivity.java
@@ -0,0 +1,79 @@
+package com.example.covidcaster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HelpActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_help);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_help);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view_help);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_map:
+                intent = new Intent(this, RegionalDataActivity.class);
+                break;
+            case R.id.nav_graph:
+                intent = new Intent(this, DataActivity.class);
+                break;
+            case R.id.nav_news:
+                intent = new Intent(this, NewsActivity.class);
+                break;
+            case R.id.nav_home:
+                intent = new Intent(this, MainActivity.class);
+                break;
+            default:
+                intent = new Intent(this, HelpActivity.class);
+        }
+        startActivity(intent);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_help);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_help);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
